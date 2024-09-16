acf9eb7119d84578e34bfa9e24fea5c8dcf92ec6
Alex Forshtat <forshtat1@gmail.com>
2024-09-16 14:19:20 +0200

    AA-446: Separate 'nonce' into two 256-bit values 'nonceKey' and 'nonceSequence'

--- core/rip7712_nonce.go
+++ core/rip7712_nonce.go
@@ -9,13 +9,13 @@ import (
 )
 
 // TODO: accept address as configuration parameter
-var AA_NONCE_MANAGER = common.HexToAddress("0x63f63e798f5F6A934Acf0a3FD1C01f3Fac851fF0")
+var AA_NONCE_MANAGER = common.HexToAddress("0x2A9D5743c62dC9420ca7B40E443C966a9527E762")
 
 func prepareNonceManagerMessage(tx *types.Rip7560AccountAbstractionTx) []byte {
 
 	return slices.Concat(
 		tx.Sender.Bytes(),
-		math.PaddedBigBytes(tx.NonceKey, 24),
-		math.PaddedBigBytes(big.NewInt(int64(tx.Nonce)), 8),
+		math.PaddedBigBytes(tx.NonceKey, 32),
+		math.PaddedBigBytes(big.NewInt(int64(tx.Nonce)), 32),
 	)
 }
