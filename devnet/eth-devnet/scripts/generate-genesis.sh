a7450c2d0af17db1da7d3e0046197abbbe3727bc
shahafn <shahaflol@gmail.com>
2024-09-23 16:47:52 +0300

    Updating Electra (Prague) fork epoch
    Geth cannot handle Prauge payloads yet, so no need in Prague fork yet

--- devnet/eth-devnet/scripts/generate-genesis.sh
+++ devnet/eth-devnet/scripts/generate-genesis.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 PRYSM_CTL=../prysmctl-v5.1.0-linux-amd64
 
-$PRYSM_CTL testnet generate-genesis --fork deneb --num-validators 2 --genesis-time-delay 45 --chain-config-file ./config.yml --geth-genesis-json-in ./genesis.json.template  --geth-genesis-json-out ./genesis.prysm.json --output-ssz ./genesis.ssz
+$PRYSM_CTL testnet generate-genesis --fork deneb --num-validators 2 --genesis-time-delay 30 --chain-config-file ./config.yml --geth-genesis-json-in ./genesis.json.template  --geth-genesis-json-out ./genesis.prysm.json --output-ssz ./genesis.ssz
 
 cp genesis.prysm.json genesis.ssz ../node1/data
 cp genesis.prysm.json genesis.ssz ../node2/data
